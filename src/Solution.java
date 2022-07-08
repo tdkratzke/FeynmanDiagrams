6affcf9d84a9c2f610cc1fb131fac3ecf90ac24a
Thal <tdkratzke@gmail.com>
2022-07-08 01:10:23 -0400

    //tdkratzke@gmail.com

---
+++ src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long testCase = input.nextLong();
+        input.close();
+        System.out.println((long)(lines(testCase, ((testCase/2)+1))/Math.pow(2, (double)testCase/2)));
+    }
+
+    static long lines(long testCase, long halfTestCase){
+
+        if(testCase == halfTestCase){
+            return halfTestCase;
+        }
+        else{
+            return (testCase * lines(testCase - 1, halfTestCase));
+        }
+    }
+}
