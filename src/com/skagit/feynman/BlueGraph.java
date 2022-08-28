142386993c15a7897c086467af567a0bb6e26394
Thomas Kratzke <tkratzke@gmail.com>
2022-08-28 13:24:30 -0500

    New Tricks.

--- src/com/skagit/feynman/BlueGraph.java
+++ src/com/skagit/feynman/BlueGraph.java
@@ -72,26 +72,30 @@ public class BlueGraph {
 
 		/** Count ways to match pair into components that are connectedToPath. */
 		if (_nUnmatchedInConnectedToPath > 2) {
-			for (int k0X = _connectedToPathComponents.nextSetBit(0); k0X >= 0; k0X = _connectedToPathComponents
-					.nextSetBit(k0X + 1)) {
-				final BitSet matchedNodes = _matchedNodesS[k0X];
-				final int size = _sizes[k0X];
-				final int cardinality = matchedNodes.cardinality();
-				if (size == cardinality) {
-					continue;
-				}
-				final int k1X = matchedNodes.nextClearBit(k0X == k0 ? (k1 + 1) : 0);
-				final int[] pairX = new int[] {
-						k0X, k1X
-				};
+			final boolean useNewTricks = true;
+			if (useNewTricks) {
+				final int[] pairX = getNodeToMatch(/* afterPair= */pair);
 				match(pair, pairX);
 				final int thisCount = recursiveGetCount(nRedEdgesPlaced + 1);
 				unMatch(pair, pairX);
-				if (true) {
-					count += (_nUnmatchedInConnectedToPath - 1) * thisCount;
-					break;
-				} else {
+				count += (_nUnmatchedInConnectedToPath - 1) * thisCount;
+			} else {
+				for (int k0X = _connectedToPathComponents.nextSetBit(0); k0X >= 0; k0X = _connectedToPathComponents
+						.nextSetBit(k0X + 1)) {
+					final BitSet matchedNodes = _matchedNodesS[k0X];
+					final int size = _sizes[k0X];
+					final int cardinality = matchedNodes.cardinality();
+					if (size == cardinality) {
+						continue;
+					}
+					final int k1X = matchedNodes.nextClearBit(k0X == k0 ? (k1 + 1) : 0);
+					final int[] pairX = new int[] {
+							k0X, k1X
+					};
 					final int multiplier = size - cardinality - (k0 == k0X ? 1 : 0);
+					match(pair, pairX);
+					final int thisCount = recursiveGetCount(nRedEdgesPlaced + 1);
+					unMatch(pair, pairX);
 					count += multiplier * thisCount;
 				}
 			}
