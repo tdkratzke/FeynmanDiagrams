bd0cf827836657e7922d7ab51da70707c12387c9
Thomas Kratzke <tkratzke@gmail.com>
2022-09-16 11:14:45 -0500

    More progress on speeding up ParFeynmanF.

--- src/com/skagit/feynman/FeynmanF.java
+++ src/com/skagit/feynman/FeynmanF.java
@@ -11,55 +11,72 @@ public class FeynmanF {
 
 	final static long _Modulo = 1000000007;
 	final static boolean _Debug = true;
-	final static long _MillisInterval = 5000L;
+	final static long _MillisInterval = 10000L;
+	// final static int _LoN = 4, _HiN = 26;
+	final static int _LoN = 50000, _HiN = 50000;
+	final static boolean _RunCrude = false;
+	final static boolean _RunF = false;
+	final static boolean _RunPar = true;
 
 	protected final int _nStar;
-	private long _OldMillis;
+	protected long _alpha[], _bravo[];
 
 	FeynmanF(final int nStar) {
 		_nStar = nStar;
+		_alpha = new long[_nStar];
+		_bravo = new long[_nStar];
 	}
 
 	public long compute() {
-		long[] alpha = new long[_nStar];
-		Arrays.fill(alpha, 1);
-		alpha[_nStar - 2] = 0;
-		_OldMillis = System.currentTimeMillis();
-		for (int n = _nStar; n > 2; n -= 2) {
-			alpha = getBravo(alpha);
-			final long currentMillis = System.currentTimeMillis();
-			if (_Debug && currentMillis >= _OldMillis + _MillisInterval) {
-				System.out.printf("\n%s %d: %s", formatCurrentTime(), n, getString(alpha));
-				_OldMillis = currentMillis;
+		Arrays.fill(_alpha, 1);
+		_alpha[_nStar - 2] = 0;
+		long oldMillis = System.currentTimeMillis();
+		final String startTimeString = formatCurrentTime();
+		int oldN = _nStar;
+		for (int alphaN = _nStar; alphaN > 2; alphaN -= 2) {
+			if (_Debug) {
+				final long millis = System.currentTimeMillis();
+				if ((millis >= oldMillis + _MillisInterval) || alphaN >= _nStar - 10 || alphaN < 100) {
+					final double secs = (millis - oldMillis) / 1000.0;
+					final int nDone = oldN - alphaN;
+					final double avg = secs > 0d ? (nDone / secs) : 0d;
+					oldN = alphaN;
+					System.out.printf("%s n[%d], %d done in %.3f seconds (avg=%.3f/sec).\n", //
+							formatCurrentTime(), alphaN, nDone, secs, avg);
+					oldMillis = System.currentTimeMillis();
+				}
 			}
+			fillInBravo(alphaN);
+			final long[] charlie = _alpha;
+			_alpha = _bravo;
+			_bravo = charlie;
 		}
-		return alpha[1];
+		System.out.printf("\n\nStarted at %s, finished at %s", startTimeString, formatCurrentTime());
+		return _alpha[1];
 	}
 
-	protected long[] getBravo(final long[] alpha) {
-		final int n = alpha.length;
-		final long[] bravo = new long[n - 2];
-		Arrays.fill(bravo, 0L);
-		for (int k = 1; k <= n; ++k) {
-			final int nInCycles = n - k;
+	protected void fillInBravo(final int alphaN) {
+		final int bravoN = alphaN - 2;
+		Arrays.fill(_bravo, 0, bravoN, 0L);
+		for (int k = 1; k <= alphaN; ++k) {
+			final int nInCycles = alphaN - k;
 			if (nInCycles == 1) {
 				continue;
 			}
-			final long multiplier = alpha[k - 1];
+			final long multiplier = _alpha[k - 1];
 			if (k > 2) {
 				final int i = k - 3;
-				bravo[i] = (bravo[i] + multiplier * (k - 1)) % _Modulo;
+				_bravo[i] = (_bravo[i] + multiplier * (k - 1)) % _Modulo;
 			}
-			final int maxCycleLen = n - k;
+			final int maxCycleLen = alphaN - k;
 			for (int cycleLen = 2; cycleLen <= maxCycleLen; ++cycleLen) {
-				if (k + cycleLen == n - 1) {
+				if (k + cycleLen == alphaN - 1) {
 					continue;
 				}
 				final int i = k + cycleLen - 3;
-				bravo[i] = (bravo[i] + multiplier) % _Modulo;
+				_bravo[i] = (_bravo[i] + multiplier) % _Modulo;
 			}
 		}
-		return bravo;
 	}
 
 	static String formatCurrentTime() {
@@ -68,13 +85,12 @@ public class FeynmanF {
 		return zonedDateTime.format(_TimeFormatter);
 	}
 
-	static String getString(final long[] longArr) {
+	static String getString(final long[] longArr, final int len) {
 		if (true) {
 			return "";
 		}
-		final int n = longArr == null ? 0 : longArr.length;
 		String s = "[";
-		for (int k = 0; k < n; ++k) {
+		for (int k = 0; k < len; ++k) {
 			s += String.format((k == 0 ? "" : ",") + "%d", longArr[k]);
 		}
 		s += "]";
@@ -83,16 +99,15 @@ public class FeynmanF {
 
 	@SuppressWarnings("unused")
 	public static void main(final String[] args) {
-		System.out.printf("\n%s", formatCurrentTime());
-		for (int nStar = 50000; nStar <= 50000; nStar += 2) {
+		for (int nStar = _LoN; nStar <= _HiN; nStar += 2) {
 			final FeynmanF feynmanF = new FeynmanF(nStar);
 			final FeynmanF parFeynmanF = new ParFeynmanF(nStar);
 			final FeynmanF crudeFeynmanF = new CrudeFeynmanF(nStar);
-			final long crudeF = 0; // crudeFeynmanF.compute();
-			final long f = feynmanF.compute();
-			final long parF = parFeynmanF.compute();
+			final long crudeF = _RunCrude ? crudeFeynmanF.compute() : 0L;
+			final long f = _RunF ? feynmanF.compute() : 0L;
+			final long parF = _RunPar ? parFeynmanF.compute() : 0L;
 			System.out.printf("\n%s nStar[%d] crudeF[%d] F[%d] parF[%d]\n", //
-					formatCurrentTime(), nStar, f, parF, crudeF);
+					formatCurrentTime(), nStar, crudeF, f, parF);
 		}
 	}
 }
--- src/com/skagit/feynman/ParFeynmanF.java
+++ src/com/skagit/feynman/ParFeynmanF.java
@@ -1,7 +1,5 @@
 package com.skagit.feynman;
 
-import java.util.Arrays;
-
 public class ParFeynmanF extends FeynmanF {
 	final static int _NWorkers = 3;
 
@@ -10,38 +8,39 @@ public class ParFeynmanF extends FeynmanF {
 	}
 
 	@Override
-	protected long[] getBravo(final long[] alpha) {
-		final int n = alpha.length;
-		final long[] bravo = new long[n - 2];
-		Arrays.fill(bravo, 0L);
+	protected void fillInBravo(final int alphaN) {
 		for (int i = 0; i < _NWorkers; ++i) {
-			updateBravoSlice(alpha, bravo, i);
+			updateBravoSlice(alphaN, i);
 		}
-		return bravo;
 	}
 
-	private void updateBravoSlice(final long[] alpha, final long[] bravo, final int i) {
-		final int n = bravo.length;
-		for (int ii = i; ii < n; ii += _NWorkers) {
-			/** ii == n-2 leaves exactly 1 vertex in the cycles. Can't have that. */
-			if (ii == n - 2) {
+	private void updateBravoSlice(final int alphaN, final int i) {
+		final int bravoN = alphaN - 2;
+		for (int ii = i; ii < bravoN; ii += _NWorkers) {
+			/** ii == bravoN - 2 leaves exactly 1 vertex in the cycles. Can't have that. */
+			if (ii == bravoN - 2) {
+				_bravo[ii] = 0L;
 				continue;
 			}
-			/** Update each ii for open/open red edges. */
-			bravo[ii] = (bravo[ii] + alpha[ii + 2] * (ii + 2)) % _Modulo;
+			/**
+			 * Update each ii for open/open red edges. Note that we're initializing
+			 * _bravo[ii] here.
+			 */
+			_bravo[ii] = (_alpha[ii + 2] * (ii + 2)) % _Modulo;
 			/**
 			 * <pre>
 			 * Update each ii for open/cycle red edges.
 			 * ii + 1 = (k + 1) + cycleLen - 2.
 			 * k + cycleLen - 2 = ii.
 			 * cycleLen = ii + 2 - k
-			 * Since k < n + 2, cycleLen > ii + 2 - (n + 2) = ii - n or cycleLen >= ii - n + 1.
+			 * Since k < bravoN + 2,
+			 *   cycleLen > ii + 2 - (bravoN + 2) = ii - n or cycleLen >= ii - n + 1.
 			 * Since k >= 0, cycleLen <= ii + 2
 			 * </pre>
 			 */
-			for (int cycleLen = Math.max(2, ii - n + 1); cycleLen <= ii + 2L; ++cycleLen) {
+			for (int cycleLen = Math.max(2, ii - bravoN + 1); cycleLen <= ii + 2L; ++cycleLen) {
 				final int k = ii - cycleLen + 2;
-				bravo[ii] = (bravo[ii] + alpha[k]) % _Modulo;
+				_bravo[ii] = (_bravo[ii] + _alpha[k]) % _Modulo;
 			}
 		}
 	}
