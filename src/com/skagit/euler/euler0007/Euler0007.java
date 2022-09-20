4b67bfe1a2f6d77aa489d9d009c4171a36f27e4d
Thomas Kratzke <tkratzke@gmail.com>
2022-09-20 08:31:11 -0500

    More cleanup of 0781 and 0007.

--- src/com/skagit/euler/euler0007/Euler0007.java
+++ src/com/skagit/euler/euler0007/Euler0007.java
@@ -4,29 +4,28 @@ import java.util.BitSet;
 
 public class Euler0007 {
 
-	static private int _TargetNumber = 10001;
+	static private int _N = 10001;
 
-	static int getNthPrime(final int nStar) {
+	static int getNthPrime(final int n) {
 		/** From wiki, we get that a good guess for the nth prime is n*ln(n). */
-		for (int k = (int) (nStar / Math.log(nStar));; k *= 2) {
+		for (int k = (int) (n / Math.log(n));; k *= 2) {
 			final BitSet bitSet = getPrimesUpTo(k);
-			if (bitSet.cardinality() >= _TargetNumber) {
+			if (bitSet.cardinality() >= _N) {
 				int prime = bitSet.nextSetBit(0);
-				for (int j = 1; j < nStar; prime = bitSet.nextSetBit(prime + 1), ++j) {
+				for (int j = 1; j < n; prime = bitSet.nextSetBit(prime + 1), ++j) {
 				}
 				return prime;
 			}
 		}
 	}
 
+	/** The sieve. */
 	private static BitSet getPrimesUpTo(final int n) {
 		final BitSet primes = new BitSet(1 + n);
 		primes.set(2, n + 1);
 		for (int k = primes.nextSetBit(2); k >= 0; k = primes.nextSetBit(k + 1)) {
-			for (int kk = k;;) {
-				if (kk > n - k) {
-					break;
-				}
+			final int upperLimit = n - k;
+			for (int kk = k; kk <= upperLimit;) {
 				kk += k;
 				primes.clear(kk);
 			}
@@ -35,8 +34,8 @@ public class Euler0007 {
 	}
 
 	public static void main(final String[] args) {
-		final int answer = getNthPrime(_TargetNumber);
-		System.out.printf("\n%d-th prime is %d.", _TargetNumber, answer);
+		final int answer = getNthPrime(_N);
+		System.out.printf("%d-th prime is %d.", _N, answer);
 	}
 
 }
--- src/com/skagit/euler/euler0781/Euler0781.java
+++ src/com/skagit/euler/euler0781/Euler0781.java
@@ -6,25 +6,26 @@ public class Euler0781 {
 	final static int _Modulo = 1000000007;
 
 	public static long feynmanF(final int nStar) {
-		int[] alpha = new int[nStar], bravo = new int[nStar], charlie;
+		int[] alpha = new int[nStar];
 		Arrays.fill(alpha, 1);
 
 		for (int alphaN = nStar; alphaN > 2; alphaN -= 2) {
 			final int bravoN = alphaN - 2;
-			long cum = 0L;
+			final int[] bravo = new int[bravoN];
+			int cum = 0;
 			for (int i = 0; i < bravoN; ++i) {
 				cum = (cum + alpha[i]) % _Modulo;
 				bravo[i] = (int) ((alpha[i + 2] * (i + 2L) + cum) % _Modulo);
 			}
-			charlie = alpha;
 			alpha = bravo;
-			bravo = charlie;
 		}
 		return alpha[1];
 	}
 
 	public static void main(final String[] args) {
 		final int nStar = 50000;
+		final long millis = System.currentTimeMillis();
 		System.out.printf("nStar[%d] f[%d]", nStar, feynmanF(nStar));
+		System.out.printf(".\tTook %d millis.", System.currentTimeMillis() - millis);
 	}
 }
